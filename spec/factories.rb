e1bcb96f3afe8d7fb02850b25ce511ad60cd54e0
Jonathan Wrobel <jwrobel@greatschools.org>
2016-11-14 21:57:54 -0800

    Added restaurant show page with sources listed
      - added restaurant source page with restaurant name, address, and
        sources list
      - added ability to make restaurant factory with sources

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,4 @@
 FactoryGirl.define do
-  factory :restaurant do
-    name "Park burger"
-    phone "1510555555"
-    display_address ["1234 main st", "Neighborhood", "Oakland, CA 94606"]
-    coordinate { { latitude: 37.8072590528299, longitude: -122.222015729986 } }
-  end
-
   factory :source do
     name "Marin Sun Farms"
     url "http://marinsunfarms.com"
@@ -13,5 +6,21 @@ FactoryGirl.define do
 
   factory :meat do
     name "Beef"
+  end
+
+  factory :restaurant do
+    name "Park burger"
+    phone "1510555555"
+    display_address ["1234 main st", "Neighborhood", "Oakland, CA 94606"]
+    coordinate { { latitude: 37.8072590528299, longitude: -122.222015729986 } }
+    sequence :api_id do |n|
+      n
+    end
+
+    trait :with_source do
+      after(:create) do |restaurant|
+        restaurant.sources << create(:source)
+      end
+    end
   end
 end
