41a5fc78cd56d32336dca134e28c9dd3a35104e8
Jonathan Wrobel <jwrobel@greatschools.org>
2016-11-26 20:10:11 -0800

    Add sources show page to display a single source
      - source links on restaurant page should link to source page

--- spec/features/vistor_views_goodmeats_restaurant_spec.rb
+++ spec/features/vistor_views_goodmeats_restaurant_spec.rb
@@ -9,9 +9,17 @@ feature "Vistor views goodmeat restaurant" do
     expect(page).to have_css "h1", text: restaurant.name
     expect(page).to have_css ".contact", text: restaurant.display_address.first
     expect(page).to have_css ".sources"
+    source_name = restaurant.sources.first.name
+
     within ".sources" do
       expect(page).to have_css ".sources__header", text: "Sources"
-      expect(page).to have_css ".source", text: restaurant.sources.first.name
+      expect(page).to have_css ".source", text: source_name
     end
+    navigate_to_source(source_name)
+    expect(page).to have_css "h1", text: source_name
+  end
+
+  def navigate_to_source(source_name)
+    page.click_on(source_name)
   end
 end
